f85b6ddf52cf20928da83ad3442a3791e4488f95
Muhammad Anas <muhammadanaswayanad@gmail.com>
2017-12-11 19:08:04 +0530

    Fixing grammatical things

--- bot/main.py
+++ bot/main.py
@@ -25,24 +25,24 @@ def handle(msg):
                        'Hi', 'Hey', 'Howdy', 'Hello', '👋']
 
     msg1 = ['do you know me', 'you know me', 'do you know my name', 'know me',
-                 'what is my name', 'who am i', 'my name']
+            'what is my name', 'who am i', 'my name']
     reply_msg1 = ['howdy ', 'How are you ',
-                       'Hi ', 'Hey ', 'Howdy ', 'Hello ', 'You are my Friend ', '👋 ']
+                  'Hi ', 'Hey ', 'Howdy ', 'Hello ', 'You are my Friend ', '👋 ']
 
     msg2 = ['❤️', 'i love you', 'love you', 'love u', 'i love u']
     reply_msg2 = ['😳 No.... ', 'Let me be your friend  🤝 ',
-                       'Love you to... ', 'I am your personal assistant. I cant love you ', 'What the hell you talking ', '❤️ ', 'You are my Friend ', 'sorry ! i am not interested  ']
+                  'Love you to... ', 'I am your personal assistant. I cant love you ', 'What the hell are you talking about ? ', '❤️ ', 'You are my Friend ', 'sorry ! i am not interested  ']
 
     msg3 = ['made ❤️ with', 'who developed you', 'who developed you?',
-                'who developed you ?', 'developers',
-                'makers', 'who maked you', 'developer team']
+            'who developed you ?', 'developers',
+            'makers', 'who maked you', 'developer team']
 
     msg4 = ['how are you', 'how hope you', 'how do you do',
-                'how are u', 'how hope u', 'how do u do',
-                'how are you ?', 'how hope you ?', 'how do you do ?',
-                'how are u ?', 'how hope u ?', 'how do u do ?'
-                'how are you?', 'how hope you?', 'how do you do?',
-                'how are u?', 'how hope u?', 'how do u do?']
+            'how are u', 'how hope u', 'how do u do',
+            'how are you ?', 'how hope you ?', 'how do you do ?',
+            'how are u ?', 'how hope u ?', 'how do u do ?'
+            'how are you?', 'how hope you?', 'how do you do?',
+            'how are u?', 'how hope u?', 'how do u do?']
 
     if command in greetings:
         idx = randint(0, reply_greetings.__len__() - 1)
@@ -58,7 +58,7 @@ def handle(msg):
 
     elif command == '/start':
         bot.sendMessage(
-            chat_id, 'Hello '+username)
+            chat_id, 'Hello ' + username)
 
     elif command in msg2:
         idx = randint(0, reply_msg2.__len__() - 1)
@@ -72,11 +72,11 @@ def handle(msg):
         bot.sendMessage(
             chat_id, 'Believe it or not i can download your hallticket/results for you 😁')
 
-    elif command in msg3 :
+    elif command in msg3:
         bot.sendMessage(chat_id, 'Well, that is a good thing to ask')
         bot.sendMessage(
             chat_id, 'Team Four_BitZz developed me as their final year project, They are awesome !')
-    elif command in msg3 :
+    elif command in msg3:
         bot.sendMessage(chat_id, 'I am Fine.')
         bot.sendMessage(chat_id, 'And You')
 
