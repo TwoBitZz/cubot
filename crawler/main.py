5c678b828ca90c679889397e38327092a950a240
Muhammed Anas <muhammadanaswayanad@gmail.com>
2017-12-18 06:09:05 +0530

    nothing

--- crawler/main.py
+++ crawler/main.py
@@ -1,6 +1,19 @@
+# -*- coding: latin-1 -*-
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+import time
+import datetime
+import mysql.connector as mariadb
+# database connection
+db = mariadb.connect(user="root", password="", database="cubot")
+# create a cursor for the select
+cur = db.cursor()
+
 import BeautifulSoup as soup
 from urllib2 import urlopen as ureq
 uocnot = 'http://www.universityofcalicut.info/index2.php?option=com_content&task=view&id=744'
 uocwebclient = ureq(uocnot)
 uoctimetable = 'http://www.universityofcalicut.info/index2.php?option=com_content&task=view&id=745'
-//prepare db
+#cur.execute("INSERT INTO cubot.user(chatid,name) VALUES (%s,%s)", (chat_id, username))
+# db.commit()
