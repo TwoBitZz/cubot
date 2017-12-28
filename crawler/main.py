1c123a2de2c3676d93a4118474f2247b12935eae
Muhammed Anas <muhammadanaswayanad@gmail.com>
2017-12-28 12:26:51 +0530

    saro var aam

--- crawler/main.py
+++ crawler/main.py
@@ -6,14 +6,20 @@ import time
 import datetime
 import mysql.connector as mysql
 # database connection
-db = mysqldb.connect(user="root", password="", database="cubot")
+db = mysql.connect(user="root", password="", database="cubot")
 # create a cursor for the select
 cur = db.cursor()
 
 import BeautifulSoup as soup
+import urllib2
 from urllib2 import urlopen as ureq
 uocnot = 'http://www.universityofcalicut.info/index2.php?option=com_content&task=view&id=744'
 uocwebclient = ureq(uocnot)
 uoctimetable = 'http://www.universityofcalicut.info/index2.php?option=com_content&task=view&id=745'
+uocnotdata = ureq(uocnot).read()
+uocwebclient.close()
+uocnotcont = soup(uocnotdata, 'html.parser')
+st = uocnotcont.td.string
+print(st)
 #cur.execute("INSERT INTO cubot.user(chatid,name) VALUES (%s,%s)", (chat_id, username))
 # db.commit()
